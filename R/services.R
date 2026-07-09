2bf0bd44d079aff14a210d83e9c7358abe4a8638
GOBCAN\ejorgon <ejorgon@gobiernodecanarias.org>
2026-07-09 12:31:51 +0100

    style: remove comments from services.R

--- R/services.R
+++ R/services.R
@@ -6,11 +6,7 @@ require(httr)
 .pkg_env$api_root_url <- "https://datos.canarias.es/api/estadisticas/"
 .pkg_env$api_version <- "1.0"
 
-# Change the API_KEY name to avoid confusion.
-# - The API_KEY content does not refer to an access token.
-# - It's actually used to redirect queries.
-# ISTAC_API_KEY = 'dWcm6Nn4xVO3JyAsfnGMSg5Cm2lmDQpSj73vYAzLFuswu5X1fwbuGSXnB5iLaTiT'
-# IBESTAT_API_KEY = 'ycpbugocdtSCeHYqVBkueEqipWyEQlY6KiviEPfKR2uDmvExY20eZm5VREyOM9P1'
+
 VALUE_ERROR = 'NaN'
 DEBUG = FALSE
 
@@ -37,17 +33,6 @@ build_query <- function(query_list) {
   }
   URLencode(result)
 }
-
-# get_api_key <- function(url) {
-#   api_key <- ""
-#   if(grepl("canarias", url, fixed=TRUE)) {
-#     api_key <- ISTAC_API_KEY
-#   }
-#   if(grepl("ibestat", url, fixed=TRUE)) {
-#     api_key <- IBESTAT_API_KEY
-#   }
-#   return(api_key)
-# }
 
 get_api_key <- function(url) {
 
